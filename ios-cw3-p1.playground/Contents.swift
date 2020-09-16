f4f6f57a1dc181e027dcbf8f08173ba7eaead2e5
Farah-AlBader <70542682+Farah-AlBader@users.noreply.github.com>
2020-09-16 20:36:26 +0300

    done!

---
+++ ios-cw3-p1.playground/Contents.swift
@@ -0,0 +1,6 @@
+func PrintMe(){
+    print("name: Farah Hani AlBader")
+    print("age: 16")
+    print("hobbies: 1-watchimg movies/tv shows \n \t\t 2-listening to music")
+}
+ PrintMe()
---
+++ ios-cw3-p2.playground/Contents.swift
@@ -0,0 +1,10 @@
+
+var number1 = 5
+var number2 = 3
+
+func add (a: Int, b:Int) -> Int {
+    return(number1 + number2)
+}
+let fuctionCall = add(a: number1, b: number2)
+
+print(fuctionCall)
